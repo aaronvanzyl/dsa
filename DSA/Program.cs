88e87f4cbb9e835fefd2d232ae68f44f273ea11e
aaronvanzyl <aaron@vanzyl.org>
2020-07-26 18:18:54 -0700

    Create prefix tree structure

---
+++ DSA/Program.cs
@@ -0,0 +1,15 @@
+﻿using DSA.course6.week1;
+using System;
+using System.Xml.Serialization;
+using Util;
+
+namespace DSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AssembleErrorFree.Run();
+        }
+    }
+}
---
+++ DSA/course6/week1/AssembleErrorFree.cs
@@ -0,0 +1,17 @@
+﻿using Util.Tree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.course6.week1
+{
+    class AssembleErrorFree
+    {
+        public static void Run() {
+            string[] strings = new string[] { "AAGGG", "ACTTT", "AGGCT", "GCCAC", "TCCGC" };
+            PrefixTree tree = new PrefixTree(strings);
+            tree.PrintTree();
+        }
+
+    }
+}
